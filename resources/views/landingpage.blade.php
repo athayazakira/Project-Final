364c3c3129a5a4ae8af979485b9f852dae598701
athayazakira <90587355+athayazakira@users.noreply.github.com>
2022-07-11 22:32:20 +0700

    Add files via upload

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Website Tanya Jawab</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+	<!----hero Section start---->
+
+	<div class="hero">
+		<nav>
+			<h3 class="logo">Tanya<span>Jawab</span></h3>
+		</nav>
+		<section id="home" class="mt-5">
+     <div class="container py-3">
+                <div class="row align-items-center">
+                    <div class="col-md-6 order-md-1 text-md-end mb-5 mb-md-0" data-aos="zoom-in" data-aos-delay="100ms">
+                        <img src="img/landingpage.png" alt="" width="650px">
+     </div>
+
+		<div class="content">
+			<h4><center>Hello friends, Welcome to Website</center></h4>
+			<h1>Tanya <span>Jawab</span></h1>
+			<h3><center>Bertanya Disini Sepuas Anda<center></h3>
+				<button type="button">Login</button>
+			<div class="newslatter">
+			</div>
+		</div>
+	</div>
+
+	<!----About section start---->
+	<section class="about">
+		<div class="main">
+			<div class="about-text">
+				<h2>About</h2>
+				<h5>Tanya<span>Jawab</span></h5>
+				<p>Apapun itu tanyakan disini</p>
+				<button type="button">Let's Talk</button>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
